72029f86eba79bdb4daf253b930d83bfc92307a6
Jan Throm <Jan.Throm@TecAlliance.net>
2022-10-17 15:09:26 +0200

    created base project for carpool web API #2

---
+++ CarPoolApi/CarPoolApi/Controllers/CarPoolController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarPoolApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarPoolController : ControllerBase
+    {
+       
+    }
+}
